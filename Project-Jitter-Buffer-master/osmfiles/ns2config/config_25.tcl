a3a7b77347a55f6cdb78595dd6a88de8e1be8032
Adi Saputra <adisaputra@pop-os.localdomain>
2020-02-28 13:30:48 +0800

    masih error script-n, perulangannya

--- Project-Jitter-Buffer-master/osmfiles/ns2config/config_25.tcl
+++ Project-Jitter-Buffer-master/osmfiles/ns2config/config_25.tcl
@@ -6,8 +6,8 @@ set opt(start) 0.0
 set opt(stop) 300.0
 
 # set floor size
-set opt(x) 2747.17
-set opt(y) 3342.26
-set opt(min-x) 2046.17
-set opt(min-y) 2667.46
+set opt(x) 2747.18
+set opt(y) 3342.23
+set opt(min-x) 2046.19
+set opt(min-y) 2667.49
 
--- Project-Jitter-Buffer-master/osmfiles/ns2config/config_50.tcl
+++ Project-Jitter-Buffer-master/osmfiles/ns2config/config_50.tcl
@@ -6,8 +6,8 @@ set opt(start) 0.0
 set opt(stop) 300.0
 
 # set floor size
-set opt(x) 2747.18
-set opt(y) 3342.0
-set opt(min-x) 2046.18
-set opt(min-y) 2667.46
+set opt(x) 2747.17
+set opt(y) 3342.25
+set opt(min-x) 2046.15
+set opt(min-y) 2667.48
 
